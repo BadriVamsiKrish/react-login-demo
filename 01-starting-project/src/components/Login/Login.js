41ca97292c3015069533e0e59ebeef17e1f1269a
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-04 16:10:37 +0530

    using-useReducer-email

--- 01-starting-project/src/components/Login/Login.js
+++ 01-starting-project/src/components/Login/Login.js
@@ -1,47 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state,action) =>{
+  if (action.type === 'USER_INPUT'){
+    return { value: action.val, isValid: action.val.includes('@')}
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value, isValid: state.value.includes('@')}
+  }
+    return {value : '', isValid : false }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [enteredClgName,setEnteredClgName]= useState('');
   const [clgNameIsValid,setClgNameIsValid]= useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(()=>{
-    const identifier = setTimeout(() => {
-      console.log("check validity");
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClgName.trim().length > 0
-      );
-    },500);
-   return () =>{
-    console.log("clean up");
-    clearTimeout(identifier);
-   }
-  },[enteredEmail,enteredPassword,enteredClgName]);
+  const [emailState,dispatchEmail]= useReducer(emailReducer,{
+    value : '',
+    isValid : null,
+  });
+
+  // useEffect(()=>{
+  //   const identifier = setTimeout(() => {
+  //     console.log("check validity");
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClgName.trim().length > 0
+  //     );
+  //   },500);
+  //  return () =>{
+  //   console.log("clean up");
+  //   clearTimeout(identifier);
+  //  }
+  // },[enteredEmail,enteredPassword,enteredClgName]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+   dispatchEmail( {type : 'USER_INPUT', val : event.target.value} );
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    )
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-    // setFormIsValid(
-    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    // );
+   setEnteredPassword(event.target.value)
+
+    setFormIsValid(
+      (emailState.isValid && event.target.value.trim().length > 6  )
+    );
   };
   const clgNameChangeHandler = (event) =>{
     setEnteredClgName(event.target.value);
   }
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'})
   };
   const validatePasswordHandler = () => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
@@ -52,7 +72,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword,enteredClgName);
+    props.onLogin(emailState.value, enteredPassword,enteredClgName);
   };
 
   return (
@@ -60,14 +80,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
