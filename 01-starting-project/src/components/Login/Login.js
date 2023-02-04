22fcce10bac5b3495231600b12fca72b218bfbbd
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-04 15:11:44 +0530

    debouncing-Q-&-A

--- 01-starting-project/src/components/Login/Login.js
+++ 01-starting-project/src/components/Login/Login.js
@@ -14,9 +14,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClgName.trim().length > 0
-    );
+    const identifier = setTimeout(() => {
+      console.log("check validity");
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClgName.trim().length > 0
+      );
+    },500);
+   return () =>{
+    console.log("clean up");
+    clearTimeout(identifier);
+   }
   },[enteredEmail,enteredPassword,enteredClgName]);
 
   const emailChangeHandler = (event) => {
@@ -104,3 +111,37 @@ const Login = (props) => {
 };
 
 export default Login;
+
+
+/*
+What is debouncing? What is the problem with the app if we don't use debouncing?
+--> debouncing is using setTimeout function and reducing the count of http request's for every key-stroke.
+
+How does the udemy trainer achieve debouncing in his code?
+--> He achieved debouncing by using setTimeout function.
+
+When does the cleanup function get called
+--> cleanup function will get called only before the useEffect function gets called.
+
+Why do even need to have cleanup functions?
+--> we need cleanup function to delete unneccessary data to be cleaned-up.
+
+When does the useEffect hook get called in the lifecycle. Before rendering or after rendering?
+--> useEffect function only gets called after rendering of the component.
+*/
+
+/*
+When would you use useReducer and why ?
+--> When updating state depends on the other state we can use reducer hook 
+
+Why do we create the emailReducer outside the functional component and not inside it?
+--> Because it is a reducer function which takes only the component gets executed.
+
+What does the emailReducer do. Please explain
+--> Generally, emailReducer takes two arguments i.e., (state,action) and returns an object.
+state -- original state/previous state
+action -- dispatch function / new state which has to be rendered
+
+How does dispatch actions work.
+--> when we update the value or validity, we have to dispatch the action.
+*/
