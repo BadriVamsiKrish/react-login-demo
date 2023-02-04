356a2db7e8f1fc8c6e1934ca573aa6c1f2ec127f
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-04 09:26:19 +0530

    useEffect-validate- form-adding clgName-field

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,15 +6,25 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(() => {
+    const checkLoggedIn = localStorage.getItem('loggedIn')
+    if(checkLoggedIn === '1'){
+      setIsLoggedIn(true);
+    }
+  },[])
+  
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('loggedIn', "1" );
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('loggedIn');
   };
 
   return (
@@ -29,3 +39,31 @@ function App() {
 }
 
 export default App;
+
+/*
+What are side effects in React according to you
+--> side effects are the React hooks / feature that can be used to stop re-executing the code again when the state changes.
+
+What is the problem that side effect can cause.
+--> we cant execute the code again when the user wants if denpendencies are not changed.
+
+How does useEffect hook works and solve the problem
+--> useEffect hook generally consists of two arguments one is function and the other is array of dependencies,
+when the dependencies chances function gets re-executed. 
+
+What was the problem with the login which we are trying to solve
+--> It we logged-In into the page, it shows the" welcome page" but, when we refresh it automatically re-directed to the login page 
+
+What was the mechanism used to solve the problem. Explain the steps
+-->Here, to solve this problem, we use Effect hook.
+
+What does this [] mean in useEffect ( () => cbfunction, []). What does the second argument used for.
+--> [] means an array, it contains the dependencies when their state changes function will get executed again
+
+What is the dependency important in the useEffect . If we dont use dependency , do you know what would happen
+--> Yes, dependencies are important to re-execute the useEffect function when the state changes. 
+If there are no dependencies re-execution of code does not occur.
+
+Why do we have enteredEmail and enteredPassword in the dependency of useEffect
+--> since, when their state changes when the user enters in input field it state is getting changed and the function is getting called to check validity.
+*/
--- 01-starting-project/src/components/Login/Login.js
+++ 01-starting-project/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -9,23 +9,35 @@ const Login = (props) => {
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
+  const [enteredClgName,setClgName] = useState('');
+  const [clgIsValid,setClgIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClgName.trim().length > 0
+    );
+  },[enteredEmail,enteredPassword,enteredClgName])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+      event.target.value.trim().length > 6 && enteredEmail.includes('@') && enteredClgName.trim().length > 0
     );
   };
+
+  const clgNameChangeHandler = (event) =>{
+    setClgName(event.target.value);
+
+    setFormIsValid(
+      event.target.value.trim().length > 6 && enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    )
+  }
 
   const validateEmailHandler = () => {
     setEmailIsValid(enteredEmail.includes('@'));
@@ -35,9 +47,13 @@ const Login = (props) => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
+  const validateClgNameHandler = () =>{
+    setClgIsValid(enteredClgName.trim().length > 0);
+  }
+
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(enteredEmail, enteredPassword, enteredClgName);
   };
 
   return (
@@ -69,6 +85,20 @@ const Login = (props) => {
             value={enteredPassword}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
+          />
+          </div>
+        <div 
+        className={`${classes.control} ${
+          clgIsValid === false ? classes.invalid : ''
+        }`}
+        >
+          <label htmlFor="clg">College Name</label>
+          <input
+            type="text"
+            id="clg"
+            value={enteredClgName}
+            onChange={clgNameChangeHandler}
+            onBlur={validateClgNameHandler}
           />
         </div>
         <div className={classes.actions}>
